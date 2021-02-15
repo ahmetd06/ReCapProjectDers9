4722eb6a6b91cbe9acc65300cb0fe68fbe6a4a5f
ahmetd06 <ahmet.dundar@gmail.com>
2021-02-15 15:08:48 +0300

    ODEV SONU

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,31 +10,44 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-          
-            CarManager carManager = new CarManager(new  EfCarDal());
+            //CarAdd()
+            //CarGet()
+            //CarDetailsGet();
+            //CarGetByID(3);
+        }
+        void CarGetByID(int carID) {
+            CarManager carManager = new CarManager(new EfCarDal());
 
-            //Car car = new Car {  BrandId = 200, ColorId = 123, DailyPrice = 110, ModelYear = 2006, Description = "Cordoba" };
-            //carManager.Add(car);
+            var result2 = carManager.GetById(carID);
 
-            //car = new Car { BrandId = 300, ColorId = 200, DailyPrice = 150, ModelYear = 2020, Description = "Focus" };
-            //carManager.Add(car);
-
-            ////var result = carManager.GetAll();
-
-            ////foreach (var item in result)
-            ////{
-            ////    Console.WriteLine("{0} {1} {2} {3} {4}", item.Id, item.BrandId, item.ModelYear, item.DailyPrice, item.Description);
-            ////}
-
-            ////var result2 = carManager.GetById(3);
-
-            ////Console.WriteLine("id si 3 olan kayıt:{0}",result2.Description);
-
+            Console.WriteLine("id si {0} olan kayıt:{1}", carID,result2.Description);
+        }
+        void CarDetailsGet() {
+            CarManager carManager = new CarManager(new EfCarDal());
             var result3 = carManager.GetCarDetails();
 
             foreach (var item in result3)
             {
                 Console.WriteLine("{0} {1} {2} {3}", item.CarName, item.BrandName, item.ColorName, item.DailyPrice);
+            }
+        }
+        void CarAdd() {
+
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            Car car = new Car { BrandId = 200, ColorId = 123, DailyPrice = 110, ModelYear = 2006, Description = "Cordoba" };
+            carManager.Add(car);
+
+            car = new Car { BrandId = 300, ColorId = 200, DailyPrice = 150, ModelYear = 2020, Description = "Focus" };
+            carManager.Add(car);
+        }
+
+        void CarGet() {
+            CarManager carManager = new CarManager(new EfCarDal());
+            var result = carManager.GetAll();
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", item.Id, item.BrandId, item.ModelYear, item.DailyPrice, item.Description);
             }
         }
     }
